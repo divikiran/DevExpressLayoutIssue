a8b289aa01105db8a2a2675f4494047708e6b726
divikiranxam <divikiran@xam-consulting.com>
2022-02-03 16:49:04 +1100

    Initial checkin

---
+++ DevExpressControlsTest/DevExpressControlsTest/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DevExpressControlsTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            DevExpress.XamarinForms.Navigation.Initializer.Init();
+            DevExpress.XamarinForms.Editors.Initializer.Init();
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
